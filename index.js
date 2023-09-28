43acedcc1d04c51c1e7c89cd01ba3e91eb10ecc8
brevre <brendamakena09@gmail.com>
2023-09-28 18:12:46 +0200

    completed

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const divide = function () {
+    return 2000 / 100;
+}
+
+const square = (parameter) => {
+    return parameter * parameter;
+}
+
+
+const add = (param1, param2) => {
+    return param1 + param2;
+}
